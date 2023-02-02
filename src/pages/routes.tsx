02a7c634ccca20b56e77b188d8bf497fd1002ca6
DAKOTA WONG <d82wong@uwaterloo.ca>
2023-02-02 14:37:01 -0500

    Added pages for login, signup, and errors

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -1,11 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
+import Login from "./Login";
+import Signup from "./Signup"
+import ErrorPage from "./ErrorPage";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
+        errorElement: <ErrorPage/>
     },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/signup",
+        element: <Signup />,
+    }
 ]);
 
 export default router;
